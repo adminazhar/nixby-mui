eeb4ab4c30582e2f07d4946f131048ba440eda8f
Azhar Khan <admin@azhark.com>
2022-10-04 16:42:07 +0530

    cleanup index

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,15 +18,7 @@ const Home: NextPage = () => {
           aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
           cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
           at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-          Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-          numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-          asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-          assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-          soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-          ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-          Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-          delectus quo eius exercitat
+          Minima ex dolor modi inventore 
         </Typography>
       </Box>
     </Container>
